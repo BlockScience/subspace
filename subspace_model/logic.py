4c1c26009cf77b0a4a85670a1ac7f1521d4f1b01
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-20 14:34:57 -0300

    add BR logic

--- subspace_model/logic.py
+++ subspace_model/logic.py
@@ -17,7 +17,7 @@ def generic_policy(_1, _2, _3, _4) -> dict:
     return {}
 
 
-def replace_suf(variable: str) -> Callable:
+def replace_suf(variable: str, default_value=0.0) -> Callable:
     """Creates replacing function for state update from string
 
     Args:
@@ -26,10 +26,10 @@ def replace_suf(variable: str) -> Callable:
     Returns:
         function: A function that continues the state across a substep
     """
-    return lambda _1, _2, _3, state, signal: (variable, signal[variable])
+    return lambda _1, _2, _3, state, signal: (variable, signal.get(variable, default_value))
 
 
-def add_suf(variable: str) -> Callable:
+def add_suf(variable: str, default_value=0.0) -> Callable:
     """Creates replacing function for state update from string
 
     Args:
@@ -38,7 +38,7 @@ def add_suf(variable: str) -> Callable:
     Returns:
         function: A function that continues the state across a substep
     """
-    return lambda _1, _2, _3, state, signal: (variable, signal[variable] + state[variable])
+    return lambda _1, _2, _3, state, signal: (variable, signal.get(variable, default_value) + state[variable])
 
 ## Time Tracking ##
 
@@ -53,8 +53,17 @@ def s_days_passed(_1, _2, _3,
 
 ## Farmer Rewards ##
 
-def p_fund_reward(_1, _2, _3, _4) -> Signal:
-    return {}
+def p_fund_reward(_1, _2, _3, state: SubspaceModelState) -> Signal:
+    reward = state['issuance_balance'] * 0.01 # TODO
+    return {'block_reward': reward, 'issuance_balance': -reward}
 
-def p_issuance_reward(_1, _2, _3, _4) ->  Signal:
-    {}
+def p_issuance_reward(_1, _2, _3, state: SubspaceModelState) ->  Signal:
+    reward = state['fund'] * 0.01 # TODO
+    return {'block_reward': reward, 'issuance_balance': -reward}
+
+
+def p_split_reward(params: SubspaceModelParams, _2, state: SubspaceModelState, _4) ->  Signal:
+    reward = state['block_reward']
+    reward_to_fund = reward * params['reward_proposer_share'] * params['fund_tax_on_proposer_reward']
+    reward_to_farmers = reward - reward_to_fund
+    return {'farmers_balance': reward_to_farmers, 'fund_balance': reward_to_fund}
--- subspace_model/types.py
+++ subspace_model/types.py
@@ -6,7 +6,8 @@ Days = Annotated[float, 'days']  # Number of days
 Credits = Annotated[float, "SSC"]  # Filecoin currency
 PerYear = Annotated[float, "1/year"]  # Yearly rate
 Year = Annotated[float, "year"]  # Number of years
-
+Percentage = Annotated[float, '%']
+    
 class SubspaceModelState(TypedDict):
     days_passed: Days
     delta_days: Days
@@ -18,10 +19,16 @@ class SubspaceModelState(TypedDict):
     staking_pool_balance: Credits
     fund_balance: Credits
     burnt_balance: Credits
+    block_reward: Credits
 
 class SubspaceModelParams(TypedDict):
     label: str
     timestep_in_days: Days
+    reward_proposer_share: Percentage
+    # Fees & Taxes
+    fund_tax_on_proposer_reward: Percentage
+    fund_tax_on_storage_fees: Percentage
+    operator_tax_on_compute_revenue: Percentage
 
 class SubspaceModelSweepParams(TypedDict):
     label: list[str]
