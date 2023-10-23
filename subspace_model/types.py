862e57e5ae9ca9d614755bb6839ed2aba28f6bb4
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-23 18:27:54 -0300

    add env processes

--- subspace_model/types.py
+++ subspace_model/types.py
@@ -3,14 +3,21 @@ from dataclasses import dataclass
 
 
 Days = Annotated[float, 'days']  # Number of days
-Credits = Annotated[float, "SSC"]  # Filecoin currency
+Credits = Annotated[float, "SSC"] 
 PerYear = Annotated[float, "1/year"]  # Yearly rate
 Year = Annotated[float, "year"]  # Number of years
 Percentage = Annotated[float, '%']
+CreditsPerComputeUnits = Annotated[float, 'SSC/CU']
+ComputeUnits = Annotated[float, 'CU']
+Shannon = Annotated[float, "Shannon"] # 1e-18 SSC 
+
     
 class SubspaceModelState(TypedDict):
+    # Time Variables
     days_passed: Days
     delta_days: Days
+
+    # Stocks
     issuance_balance: Credits
     operators_balance: Credits
     nominators_balance: Credits
@@ -19,7 +26,16 @@ class SubspaceModelState(TypedDict):
     staking_pool_balance: Credits
     fund_balance: Credits
     burnt_balance: Credits
+
+    # Deterministic Variables
     block_reward: Credits
+
+    # Stochastic Variables
+    average_base_fee: CreditsPerComputeUnits
+    average_priority_fee: CreditsPerComputeUnits
+    average_compute_units: ComputeUnits
+    transaction_count_per_timestep: int
+
 
 class SubspaceModelParams(TypedDict):
     label: str
