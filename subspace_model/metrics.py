b9ed389d184b18423616bec30f3ed0fa85f96904
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-25 18:52:48 -0300

    sim running

--- subspace_model/metrics.py
+++ subspace_model/metrics.py
@@ -6,7 +6,7 @@ def circulating_supply(state):
             + state['farmers_balance'])
 
 def user_supply(state):
-    return state['circulating_supply'] + state['staking_pool_balance']
+    return circulating_supply(state) + state['staking_pool_balance']
 
 def issued_supply(state):
-    return state['user_supply'] + state['fund_balance'] + state['burnt_balance']
+    return user_supply(state) + state['fund_balance'] + state['burnt_balance']
