e120dfbd9474a24e5d61af810ed95b33bce16deb
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-11-08 21:56:45 -0300

    param tuning

--- subspace_model/const.py
+++ subspace_model/const.py
@@ -3,6 +3,7 @@ from subspace_model.types import *
 U32_RANGE = 2 ** 32
 U128_RANGE = 2 ** 128
 MIB_IN_BYTES = 1048576
+DAY_TO_SECONDS = (24 * 60 * 60)
 
 # Subspace Constants
 MAX_CREDIT_ISSUANCE = 3_000_000_000 # TODO: check if true
@@ -14,6 +15,6 @@ BLOCK_TIME: Seconds = 6 # As per Subnomicon. Approximate value.
 RAW_RECORD_IN_CHUNKS: Chunk = 2 ** 15 # As per Subnomicon
 PIECE_SIZE: Bytes = RAW_RECORD_IN_CHUNKS * CHUNK_SIZE + KZG_COMMITMENT + WITNESS # As per Subnomicon
 SECTOR_IN_PIECES: Piece = 1_000 # As per Subnomicon
-SECTOR_SIZE = SECTOR_IN_PIECES * PIECE_SIZE
+SECTOR_SIZE = SECTOR_IN_PIECES * PIECE_SIZE # ~= 1 MiB
 SEGMENT_SIZE: Bytes = 128 * MIB_IN_BYTES # As per discussions
 SEGMENT_HISTORY_SIZE: Bytes = SEGMENT_SIZE * 2 # As per discussions
