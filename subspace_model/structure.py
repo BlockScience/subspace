24085c7fd1867a6590ed70b41d49e28f8cedc0aa
Shawn Anderson <shawn@longtailfinancial.com>
2023-11-28 14:10:58 -0800

    Add logging infrastructure and interactive mode.

--- subspace_model/structure.py
+++ subspace_model/structure.py
@@ -1,6 +1,10 @@
-from subspace_model.logic import *
-from typing import Callable
+import logging
+
+logger = logging.getLogger('subspace-digital-twin')
 from copy import deepcopy
+from typing import Callable
+
+from subspace_model.logic import *
 
 # Non processed blocks
 
@@ -9,128 +13,101 @@ _SUBSPACE_MODEL_BLOCKS: list[dict] = [
         'label': 'Time Tracking',
         'ignore': False,
         'desc': 'Updates the time in the system',
-        'policies': {
-            'evolve_time': p_evolve_time
-        },
+        'policies': {'evolve_time': p_evolve_time},
         'variables': {
             'days_passed': s_days_passed,
             'delta_days': replace_suf,
-            'delta_blocks': s_delta_blocks
-        }
+            'delta_blocks': s_delta_blocks,
+        },
     },
     {
         'label': 'Environmental Processes',
         'ignore': False,
-        'policies': {
-            'sector_onboarding': p_pledge_sectors
-        },
+        'policies': {'sector_onboarding': p_pledge_sectors},
         'variables': {
             'average_base_fee': s_average_base_fee,
             'average_priority_fee': s_average_priority_fee,
             'average_compute_weight_per_tx': s_average_compute_weight_per_tx,
             'transaction_count': s_transaction_count,
             'average_transaction_size': s_average_transaction_size,
-            'space_pledged': add_suf
-        }
+            'space_pledged': add_suf,
+        },
     },
     {
         'label': 'Archival Process',
-        'policies': {
-            'archival': p_archive
-        },
-        'variables': {
-            'history_size': add_suf,
-            'buffer_size': add_suf
-        }
+        'policies': {'archival': p_archive},
+        'variables': {'history_size': add_suf, 'buffer_size': add_suf},
     },
     {
         'label': 'Farmer Rewards (Inflow)',
         'ignore': False,
         'policies': {
             'fund_reward': p_fund_reward,
-            'issuance_reward': p_issuance_reward
+            'issuance_reward': p_issuance_reward,
         },
         'variables': {
             'fund_balance': add_suf,
             'reward_issuance_balance': add_suf,
-            'block_reward': replace_suf
-        }
+            'block_reward': replace_suf,
+        },
     },
     {
         'label': 'Farmer Rewards (Outflow)',
         'ignore': False,
-        'policies': {
-            'split_farmer_rewards': p_split_reward
-        },
-        'variables': {
-            'farmers_balance': add_suf,
-            'fund_balance': add_suf
-        }
+        'policies': {'split_farmer_rewards': p_split_reward},
+        'variables': {'farmers_balance': add_suf, 'fund_balance': add_suf},
     },
     {
         'label': 'Operator Rewards',
         'ignore': False,
-        'policies': {
-            'operator_rewards': p_operator_reward
-        },
-        'variables': {
-            'other_issuance_balance': add_suf,
-            'operators_balance': add_suf
-        }
+        'policies': {'operator_rewards': p_operator_reward},
+        'variables': {'other_issuance_balance': add_suf, 'operators_balance': add_suf},
     },
     {
         'label': 'Storage Fees',
         'ignore': False,
-        'policies': {
-            'storage_fees': p_storage_fees
-        },
+        'policies': {'storage_fees': p_storage_fees},
         'variables': {
             'holders_balance': add_suf,
             'farmers_balance': add_suf,
             'fund_balance': add_suf,
-            'storage_fee_volume': replace_suf
-        }
+            'storage_fee_volume': replace_suf,
+        },
     },
     {
         'label': 'Compute Fees',
         'ignore': False,
-        'policies': {
-            'compute_fees': p_compute_fees
-        },
+        'policies': {'compute_fees': p_compute_fees},
         'variables': {
             'farmers_balance': add_suf,
             'operators_balance': add_suf,
             'nominators_balance': add_suf,
             'holders_balance': add_suf,
             'compute_fee_volume': replace_suf,
-            'rewards_to_nominators': replace_suf
-        }
+            'rewards_to_nominators': replace_suf,
+        },
     },
     {
         'label': 'Direct Allocations',
         'ignore': False,
-        'policies': {
-            'unvest': p_unvest
-        },
+        'policies': {'unvest': p_unvest},
         'variables': {
             'holders_balance': add_suf,
             'other_issuance_balance': add_suf,
-            'allocated_tokens': replace_suf
-        }
+            'allocated_tokens': replace_suf,
+        },
     },
     {
         'label': 'Slash',
         'ignore': False,
-        'policies': {
-            'slash': p_slash
-        },
+        'policies': {'slash': p_slash},
         'variables': {
             'staking_pool_balance': add_suf,
-            'fund_balance': add_suf, 
+            'fund_balance': add_suf,
             'holders_balance': add_suf,
             'operator_pool_shares': add_suf,
-            'burnt_balance': add_suf
-        }
+            'burnt_balance': add_suf,
+        },
     },
     {
         'label': 'Staking / Unstaking',
@@ -143,35 +120,40 @@ _SUBSPACE_MODEL_BLOCKS: list[dict] = [
             'nominators_balance': add_suf,
             'staking_pool_balance': add_suf,
             'operator_pool_shares': add_suf,
-            'nominator_pool_shares': add_suf
-        }
+            'nominator_pool_shares': add_suf,
+        },
     },
     {
         'label': 'Transfers',
         'ignore': False,
-        'policies': {
-            'transfers': p_transfers
-        },
-        'variables':  {
+        'policies': {'transfers': p_transfers},
+        'variables': {
             'operators_balance': add_suf,
             'nominators_balance': add_suf,
             'holders_balance': add_suf,
-            'farmers_balance': add_suf
-        }
-    }, 
+            'farmers_balance': add_suf,
+        },
+    },
     {
         'label': 'Metrics',
-        'policies': {
-
-        }, 
+        'policies': {},
         'variables': {
-            'circulating_supply': lambda _1, _2, _3, s, _5: ('circulating_supply', circulating_supply(s)),
+            'circulating_supply': lambda _1, _2, _3, s, _5: (
+                'circulating_supply',
+                circulating_supply(s),
+            ),
             'user_supply': lambda _1, _2, _3, s, _5: ('user_supply', user_supply(s)),
-            'issued_supply': lambda _1, _2, _3, s, _5: ('issued_supply', issued_supply(s)),
-            'sum_of_stocks': lambda _1, _2, _3, s, _5: ('sum_of_stocks', sum_of_stocks(s)),
-            'block_utilization': s_block_utilization
-        }
-    }
+            'issued_supply': lambda _1, _2, _3, s, _5: (
+                'issued_supply',
+                issued_supply(s),
+            ),
+            'sum_of_stocks': lambda _1, _2, _3, s, _5: (
+                'sum_of_stocks',
+                sum_of_stocks(s),
+            ),
+            'block_utilization': s_block_utilization,
+        },
+    },
 ]
 
 
@@ -190,3 +172,5 @@ for block in [b for b in _SUBSPACE_MODEL_BLOCKS if b.get('ignore', False) != Tru
     blocks.append(_block)
 
 SUBSPACE_MODEL_BLOCKS = deepcopy(blocks)
+
+logger.info('SUBSPACE_MODEL_BLOCKS: \n%s', [b['label'] for b in SUBSPACE_MODEL_BLOCKS])
