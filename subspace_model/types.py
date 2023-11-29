d874f71cb6685fde660611af2319d18efefba6eb
Shawn Anderson <shawn@longtailfinancial.com>
2023-11-28 21:18:08 -0800

    Include new types.

--- subspace_model/types.py
+++ subspace_model/types.py
@@ -1,34 +1,36 @@
-from typing import Annotated, TypedDict, Union, Callable, NamedTuple, Optional
 from dataclasses import dataclass
+from typing import Annotated, Callable, NamedTuple, Optional, TypedDict, Union
 
 # Time units
-Blocks = Annotated[float, 'blocks'] # 1 block ~ 6s. Or 1 day ~ 14400 blocks
+Blocks = Annotated[float, 'blocks']   # 1 block ~ 6s. Or 1 day ~ 14400 blocks
 Days = Annotated[float, 'days']  # Number of days
 Seconds = Annotated[float, 's']
-PerYear = Annotated[float, "1/year"]  # Yearly rate
-Year = Annotated[float, "year"]  # Number of years
+PerYear = Annotated[float, '1/year']  # Yearly rate
+Year = Annotated[float, 'year']  # Number of years
 
 # Measurement units
-Credits = Annotated[float, "SSC"] 
+Credits = Annotated[float, 'SSC']
 CreditsPerComputeWeights = Annotated[float, 'SSC/CW']
 CreditsPerDay = Annotated[float, 'SSC/day']
 ComputeWeights = Annotated[float, 'CW']
-Shannon = Annotated[float, "Shannon"] # 1e-18 SSC 
+Shannon = Annotated[float, 'Shannon']   # 1e-18 SSC
 ShannonPerComputeWeights = Annotated[float, 'Shannon/CW']
 
 Bytes = Annotated[int, 'bytes']
-Chunk = Annotated[int, 'chunk'] # As per Subnomicon: 1 chunk = 32 Bytes
-RawRecord = Annotated[Chunk, 'raw_record'] # As per Subnomicon: 2**15 Chunks (~1MB)
-Piece = Annotated[int, 'piece'] # As per Subnomicon: 1 piece = 1 record + commitment + witness
-Record = Annotated[Piece,'record'] # As per Subnomicon: a transformed raw record.
-Sector = Annotated[Piece, 'sector'] # As per Subnomicon: 1000 Pieces or ~ 1 GiB
+Chunk = Annotated[int, 'chunk']   # As per Subnomicon: 1 chunk = 32 Bytes
+RawRecord = Annotated[Chunk, 'raw_record']   # As per Subnomicon: 2**15 Chunks (~1MB)
+Piece = Annotated[
+    int, 'piece'
+]   # As per Subnomicon: 1 piece = 1 record + commitment + witness
+Record = Annotated[Piece, 'record']   # As per Subnomicon: a transformed raw record.
+Sector = Annotated[Piece, 'sector']   # As per Subnomicon: 1000 Pieces or ~ 1 GiB
 
 # As per Subnomicon: A collection of potential partial or full blocks.
 # Can be either a fixed-size portion of the Blockchain History
 # or a fixed-size portion of the Archived History
-Segment = Annotated[Bytes, 'segment'] 
+Segment = Annotated[Bytes, 'segment']
 RecordedHistorySegment = Annotated[Record, 'record_segment']
-ArchivedHistorySegment = Annotated[Piece, 'archive_segment'] 
+ArchivedHistorySegment = Annotated[Piece, 'archive_segment']
 
 # Misc units
 Percentage = Annotated[float, '%']
@@ -107,7 +109,7 @@ class SubspaceModelParams(TypedDict):
     # Economic Parameters
     reward_proposer_share: Percentage
     max_credit_supply: Credits
-    
+
     # Fees & Taxes
     fund_tax_on_proposer_reward: Percentage
     fund_tax_on_storage_fees: Percentage
@@ -119,43 +121,54 @@ class SubspaceModelParams(TypedDict):
     slash_to_holders: Percentage
 
     # Behavioral Parameters
-    operator_avg_stake_per_ts: Percentage
-    nominator_avg_stake_per_ts: Percentage
-    operator_std_stake_per_ts: Percentage
-    nominator_std_stake_per_ts: Percentage
+    operator_stake_per_ts_function: Callable[[bool], Percentage]
+    nominator_stake_per_ts_function: Callable[[bool], Percentage]
+    # operator_avg_stake_per_ts: Callable[[bool], Percentage]
+    # nominator_avg_stake_per_ts: Callable[[bool], Percentage]
+    # operator_std_stake_per_ts: Callable[[bool], Percentage]
+    # nominator_std_stake_per_ts: Callable[[bool], Percentage]
     transfer_farmer_to_holder_per_day: Percentage
     transfer_operator_to_holder_per_day: Percentage
     transfer_holder_to_nominator_per_day: Percentage
     transfer_holder_to_operator_per_day: Percentage
 
     # Environmental Parameters
-    avg_base_fee: Credits
-    std_base_fee: Credits
+    base_fee_function: Callable[[bool], Percentage]
+    # avg_base_fee: Credits
+    # std_base_fee: Credits
     min_base_fee: Credits
 
-    avg_priority_fee: Credits
-    std_priority_fee: Credits
+    priority_fee_function: Callable[[bool], Percentage]
+    # avg_priority_fee: Credits
+    # std_priority_fee: Credits
 
-    avg_compute_weights_per_tx: ComputeWeights
-    std_compute_weights_per_tx: ComputeWeights
+    compute_weights_per_tx_function: Callable[[bool], ComputeWeights]
+    # avg_compute_weights_per_tx: ComputeWeights
+    # std_compute_weights_per_tx: ComputeWeights
     min_compute_weights_per_tx: ComputeWeights
 
-    avg_compute_weights_per_bundle: ComputeWeights
-    std_compute_weights_per_bundle: ComputeWeights
+    compute_weights_per_bundle_function: Callable[[bool], ComputeWeights]
+    # avg_compute_weights_per_bundle: ComputeWeights
+    # std_compute_weights_per_bundle: ComputeWeights
     min_compute_weights_per_bundle: ComputeWeights
 
-    avg_transaction_size: Bytes
-    std_transaction_size: Bytes
+    transaction_size_function: Callable[[bool], Bytes]
+    # avg_transaction_size: Bytes
+    # std_transaction_size: Bytes
     min_transaction_size: Bytes
-    
-    avg_bundle_size: Bytes # TODO: confirm
-    std_bundle_size: Bytes # TODO: confirm
-    min_bundle_size: Bytes # TODO: confirm
 
-    avg_transaction_count_per_day: float
-    avg_bundle_count_per_day: float
+    bundle_size_function: Callable[[bool], Bytes]
+    # avg_bundle_size: Bytes # TODO: confirm
+    # std_bundle_size: Bytes # TODO: confirm
+    min_bundle_size: Bytes   # TODO: confirm
 
-    avg_slash_per_day: float
-    avg_new_sectors_per_day: float
-    std_new_sectors_per_day: float
-    
+    transaction_count_per_day_function: Callable[[bool], float]
+    # avg_transaction_count_per_day: float
+    bundle_count_per_day_function: Callable[[bool], float]
+    # avg_bundle_count_per_day: float
+
+    slash_per_day_function: Callable[[bool], float]
+    # avg_slash_per_day: float
+    new_sectors_per_day_function: Callable[[bool], float]
+    # avg_new_sectors_per_day: float
+    # std_new_sectors_per_day: float
