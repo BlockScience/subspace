b73a33499898e666ba0cbddc3d0c6b34ba8ecd34
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2023-12-11 18:42:11 -0800

    start issuance sweep modelling

--- subspace_model/experiments/logic.py
+++ subspace_model/experiments/logic.py
@@ -1,4 +1,5 @@
 from scipy.stats import norm, poisson  # type: ignore
+import numpy as np
 
 from subspace_model.types import (
     StochasticFunction,
@@ -8,15 +9,34 @@ from subspace_model.types import (
 
 
 def DEFAULT_ISSUANCE_FUNCTION(params: SubspaceModelParams, state: SubspaceModelState):
-    return state['reward_issuance_balance'] * 0.01   # HACK
+    g = state["block_utilization"] if state["block_utilization"] else 0
+
+    s_min = 1000
+    s_c = 10000
+    B_max = 610
+    C_max = 1000
+    P = 37000
+    F = (s_c / P) * (B_max * g)
+    s_max = max(C_max - F, 0)
+
+    a = 1000
+    c = 1
+    b = (s_min - s_max) / (np.tanh(c))
+    s = a + b * np.tanh(-c * g)
+
+    return s
 
 
-def ISSUANCE_FUNCTION(params: SubspaceModelParams, state: SubspaceModelState):
-    return state['reward_issuance_balance'] * 0.01   # HACK
+def MOCK_ISSUANCE_FUNCTION(params: SubspaceModelParams, state: SubspaceModelState):
+    return state["reward_issuance_balance"] * 0.01  # HACK
+
+
+def MOCK_ISSUANCE_FUNCTION_2(params: SubspaceModelParams, state: SubspaceModelState):
+    return state["reward_issuance_balance"] / 5 * 365  # HACK
 
 
 def DEFAULT_SLASH_FUNCTION(params: SubspaceModelParams, state: SubspaceModelState):
-    return state['staking_pool_balance'] * 0.001   # HACK
+    return state["staking_pool_balance"] * 0.001  # HACK
 
 
 def NORMAL_GENERATOR(mu: float, sigma: float) -> StochasticFunction:
--- subspace_model/types.py
+++ subspace_model/types.py
@@ -2,38 +2,38 @@ from dataclasses import dataclass
 from typing import Annotated, Callable, NamedTuple, Optional, TypedDict, Union
 
 # Time units
-Blocks = Annotated[float, 'blocks']   # 1 block ~ 6s. Or 1 day ~ 14400 blocks
-Days = Annotated[float, 'days']  # Number of days
-Seconds = Annotated[float, 's']
-PerYear = Annotated[float, '1/year']  # Yearly rate
-Year = Annotated[float, 'year']  # Number of years
+Blocks = Annotated[float, "blocks"]  # 1 block ~ 6s. Or 1 day ~ 14400 blocks
+Days = Annotated[float, "days"]  # Number of days
+Seconds = Annotated[float, "s"]
+PerYear = Annotated[float, "1/year"]  # Yearly rate
+Year = Annotated[float, "year"]  # Number of years
 
 # Measurement units
-Credits = Annotated[float, 'SSC']
-CreditsPerComputeWeights = Annotated[float, 'SSC/CW']
-CreditsPerDay = Annotated[float, 'SSC/day']
-ComputeWeights = Annotated[float, 'CW']
-Shannon = Annotated[float, 'Shannon']   # 1e-18 SSC
-ShannonPerComputeWeights = Annotated[float, 'Shannon/CW']
+Credits = Annotated[float, "SSC"]
+CreditsPerComputeWeights = Annotated[float, "SSC/CW"]
+CreditsPerDay = Annotated[float, "SSC/day"]
+ComputeWeights = Annotated[float, "CW"]
+Shannon = Annotated[float, "Shannon"]  # 1e-18 SSC
+ShannonPerComputeWeights = Annotated[float, "Shannon/CW"]
 
-Bytes = Annotated[int, 'bytes']
-Chunk = Annotated[int, 'chunk']   # As per Subnomicon: 1 chunk = 32 Bytes
-RawRecord = Annotated[Chunk, 'raw_record']   # As per Subnomicon: 2**15 Chunks (~1MB)
+Bytes = Annotated[int, "bytes"]
+Chunk = Annotated[int, "chunk"]  # As per Subnomicon: 1 chunk = 32 Bytes
+RawRecord = Annotated[Chunk, "raw_record"]  # As per Subnomicon: 2**15 Chunks (~1MB)
 Piece = Annotated[
-    int, 'piece'
-]   # As per Subnomicon: 1 piece = 1 record + commitment + witness
-Record = Annotated[Piece, 'record']   # As per Subnomicon: a transformed raw record.
-Sector = Annotated[Piece, 'sector']   # As per Subnomicon: 1000 Pieces or ~ 1 GiB
+    int, "piece"
+]  # As per Subnomicon: 1 piece = 1 record + commitment + witness
+Record = Annotated[Piece, "record"]  # As per Subnomicon: a transformed raw record.
+Sector = Annotated[Piece, "sector"]  # As per Subnomicon: 1000 Pieces or ~ 1 GiB
 
 # As per Subnomicon: A collection of potential partial or full blocks.
 # Can be either a fixed-size portion of the Blockchain History
 # or a fixed-size portion of the Archived History
-Segment = Annotated[Bytes, 'segment']
-RecordedHistorySegment = Annotated[Record, 'record_segment']
-ArchivedHistorySegment = Annotated[Piece, 'archive_segment']
+Segment = Annotated[Bytes, "segment"]
+RecordedHistorySegment = Annotated[Record, "record_segment"]
+ArchivedHistorySegment = Annotated[Piece, "archive_segment"]
 
 # Misc units
-Percentage = Annotated[float, '%']
+Percentage = Annotated[float, "%"]
 
 # Taxonomy:
 # Chunk < Record/Piece < Sector < Plot < History
@@ -100,7 +100,7 @@ class SubspaceModelParams(TypedDict):
     timestep_in_days: Days
 
     # Mechanisms to be determined
-    issuance_function: Callable[[SubspaceModelState], CreditsPerDay]
+    issuance_function: Callable
     slash_function: Callable[[SubspaceModelState], Credits]
 
     # Implementation parameters
@@ -165,7 +165,7 @@ class SubspaceModelParams(TypedDict):
     bundle_size_function: Callable[[bool], Bytes]
     # avg_bundle_size: Bytes # TODO: confirm
     # std_bundle_size: Bytes # TODO: confirm
-    min_bundle_size: Bytes   # TODO: confirm
+    min_bundle_size: Bytes  # TODO: confirm
 
     transaction_count_per_day_function: Callable[[bool], float]
     # avg_transaction_count_per_day: float
