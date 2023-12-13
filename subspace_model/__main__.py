73261f0a5976f1ff367adcdf2731c043f023c605
Shawn Anderson <shawn@longtailfinancial.com>
2023-12-12 16:13:12 -0800

    Add charting functionality and add charts data.

--- subspace_model/__main__.py
+++ subspace_model/__main__.py
@@ -1,15 +1,25 @@
 import logging
 
 logger = logging.getLogger('subspace-digital-twin')
+import glob
+
 # logging.basicConfig(filename='cadcad.log', level=logging.INFO)
 import os
 from datetime import datetime
 
 import click
 import IPython
+import pandas as pd
 import panel as pn
 
 from subspace_model import default_run_args
+from subspace_model.experiments.charts import (
+    ab_block_utilization,
+    ab_circulating_supply,
+    ab_circulating_supply_volatility,
+    ab_nominator_pool_shares,
+    ab_operator_pool_shares,
+)
 from subspace_model.experiments.experiment import (
     fund_inclusion,
     issuance_sweep,
@@ -35,8 +45,25 @@ experiments = {
     'reward_split_sweep': reward_split_sweep,
 }
 
+experiment_charts = {
+    'sanity_check_run': [],
+    'standard_stochastic_run': [
+        ab_circulating_supply,
+        ab_operator_pool_shares,
+    ],
+    'issuance_sweep': [
+        ab_circulating_supply,
+        ab_operator_pool_shares,
+        ab_nominator_pool_shares,
+        ab_block_utilization,
+        ab_circulating_supply_volatility,
+    ],
+    'fund_inclusion': [],
+    'reward_split_sweep': [],
+}
 
-def pickle_results(df, directory: str, filename: str):
+
+def write_pickle_results(df, directory: str, filename: str):
     filepath = os.path.join(directory, filename)
 
     # Check if the directory exists, create it if it doesn't
@@ -46,6 +73,78 @@ def pickle_results(df, directory: str, filename: str):
     # Save the DataFrame
     df.to_pickle(filepath, compression='gzip')
     logger.info(f'Results saved to {filepath}.')
+
+
+def find_latest_simulation(experiment: str) -> str | None:
+    pattern = f'data/simulations/{experiment}-*.pkl.gz'
+    files = glob.glob(pattern)
+
+    if experiment not in list(experiments.keys()):
+        logger.warning(
+            f'Experiment {experiment} not found. Try one of: {list(experiments.keys())}'
+        )
+        return None
+
+    if not files:
+        logger.warning(
+            f'No data found for experiment: {experiment}. Try generating data with `python -m subspace_model -e {experiment}'
+        )
+        return None
+
+    # Get the most recent file based on modification time
+    latest_file = max(files, key=os.path.getmtime)
+
+    # Get the timestamp of the experiment
+    timestamp = '-'.join(latest_file.split('-')[1:]).replace('.pkl.gz', '')
+
+    return timestamp
+
+
+def get_charts(df: pd.DataFrame, experiment: str):
+    charts = [
+        (plot.__name__, plot(df, experiment)) for plot in experiment_charts[experiment]
+    ]
+
+    return charts
+
+
+def save_charts(experiment: str):
+    logger.info(f'Visualizing experiment: {experiment}...')
+    latest_simulation_timestamp = find_latest_simulation(experiment)
+    if latest_simulation_timestamp is not None:
+        latest_simulation = (
+            f'data/simulations/{experiment}-{latest_simulation_timestamp}.pkl.gz'
+        )
+        df = pd.read_pickle(latest_simulation)
+
+        # Check if the directory exists, create it if it doesn't
+        directory = f'data/charts/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        charts = get_charts(df, experiment)
+        for (chart_name, chart) in charts:
+            logger.info(f'Generating chart {chart_name} for experiment {experiment}...')
+            figure = chart.get_figure()
+            figure.savefig(
+                f'{directory}{experiment}-{latest_simulation_timestamp}-{chart_name}.png'
+            )
+
+
+def run(experiment: str, pickle: bool):
+    logger.info(f'Executing experiment: {experiment}...')
+    experiment_run = experiments[experiment]
+    df = experiment_run()
+    logger.info(f'{experiment} executed.')
+    logger.info(df)
+
+    # Conditionally pickle the results
+    if pickle:
+        write_pickle_results(
+            df,
+            directory='data/simulations/',
+            filename=f'{experiment}-{timestamp}.pkl.gz',
+        )
 
 
 @click.command()
@@ -97,6 +196,14 @@ def pickle_results(df, directory: str, filename: str):
     is_flag=True,
     help='Run all experiments.',
 )
+@click.option(
+    '-v',
+    '--visualize',
+    'visualize',
+    default=False,
+    is_flag=True,
+    help='Visualize the most recent results of the selected experiment. Combine with -e or -a to select which experiment results to visualize.',
+)
 def main(
     experiment: str,
     pickle: bool,
@@ -104,6 +211,7 @@ def main(
     log_level: str,
     clear_cache: bool,
     run_all: bool,
+    visualize: bool,
 ) -> None:
     # Initialize logging
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -116,36 +224,20 @@ def main(
         logger.info(f'Clearing caches for all experiments.')
         pn.state.clear_caches()
 
-    # Run all experiments
+    # All experiments selected
     if run_all:
-        for experiment, experiment_run in experiments.items():
-            logger.info(f'Executing experiment: {experiment}...')
-            df = experiment_run()
-            logger.info(f'{experiment} executed.')
+        for experiment in list(experiments.keys()):
+            if visualize:
+                save_charts(experiment)
+            else:
+                run(experiment, pickle)
 
-            # Conditionally pickle the results
-            if pickle:
-                pickle_results(
-                    df,
-                    directory='data/simulations/',
-                    filename=f'{experiment}-{timestamp}.pkl.gz',
-                )
-
-    # Run the selected experiment
+    # Single experiment selected
     else:
-        logger.info(f'Executing experiment: {experiment}...')
-        experiment_run = experiments[experiment]
-        df = experiment_run()
-        logger.info(f'{experiment} executed.')
-        logger.info(df)
-
-        # Conditionally pickle the results
-        if pickle:
-            pickle_results(
-                df,
-                directory='data/simulations/',
-                filename=f'{experiment}-{timestamp}.pkl.gz',
-            )
+        if visualize:
+            save_charts(experiment)
+        else:
+            run(experiment, pickle)
 
     # Conditionally drop into an IPython shell
     if interactive:
