ba1668b6ac44412134f800c1d0d445d3eb36d4c3
Shawn Anderson <shawn@longtailfinancial.com>
2023-12-19 14:33:36 -0800

    Convert ab_circulating_supply_volatility to use plotly. Turn all the plots off for now.

--- subspace_model/experiments/charts.py
+++ subspace_model/experiments/charts.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import plotly.express as px
+import seaborn as sns
 
 from subspace_model.trajectory_metrics import window_volatility
 
@@ -9,9 +9,9 @@ from subspace_model.trajectory_metrics import window_volatility
 def ab_circulating_supply(sim_df: pd.DataFrame, experiment: str) -> None:
     fig = px.line(
         sim_df,
-        x="days_passed",
-        y="circulating_supply",
-        title=f"{experiment} - AB Test Circulating Supply",
+        x='days_passed',
+        y='circulating_supply',
+        title=f'{experiment} - AB Test Circulating Supply',
     )
     return fig
 
@@ -19,9 +19,9 @@ def ab_circulating_supply(sim_df: pd.DataFrame, experiment: str) -> None:
 def ab_operator_pool_shares(sim_df: pd.DataFrame, experiment: str) -> None:
     fig = px.line(
         sim_df,
-        x="days_passed",
-        y="operator_pool_shares",
-        title=f"{experiment} - AB Test Operator Pool Shares",
+        x='days_passed',
+        y='operator_pool_shares',
+        title=f'{experiment} - AB Test Operator Pool Shares',
     )
     return fig
 
@@ -29,9 +29,9 @@ def ab_operator_pool_shares(sim_df: pd.DataFrame, experiment: str) -> None:
 def ab_nominator_pool_shares(sim_df: pd.DataFrame, experiment: str) -> None:
     fig = px.line(
         sim_df,
-        x="days_passed",
-        y="nominator_pool_shares",
-        title=f"{experiment} - AB Test Nominator Pool Shares",
+        x='days_passed',
+        y='nominator_pool_shares',
+        title=f'{experiment} - AB Test Nominator Pool Shares',
     )
     return fig
 
@@ -39,24 +39,24 @@ def ab_nominator_pool_shares(sim_df: pd.DataFrame, experiment: str) -> None:
 def ab_block_utilization(sim_df: pd.DataFrame, experiment: str) -> None:
     fig = px.line(
         sim_df,
-        x="days_passed",
-        y="block_utilization",
-        title=f"{experiment} - AB Test Block Utilization",
+        x='days_passed',
+        y='block_utilization',
+        title=f'{experiment} - AB Test Block Utilization',
     )
     return fig
 
 
 def ab_circulating_supply_volatility(sim_df: pd.DataFrame, experiment: str) -> None:
     lst = []
-    for i, g_df in sim_df.set_index(["label", "run", "days_passed"]).groupby("run"):
+    for i, g_df in sim_df.set_index(['label', 'run', 'days_passed']).groupby('run'):
         s = window_volatility(g_df.circulating_supply.diff()).reset_index()
         lst.append(s)
 
-    df = pd.concat(lst).dropna()
-    chart = sns.lineplot(
-        df.set_index(["label", "run", "days_passed"]),
-        x="days_passed",
-        y="circulating_supply",
-        hue="label",
-    ).set(title=f"{experiment} - AB Test Windowed Volatility of Circulating Supply")
-    return chart[0]
+    sim_df = pd.concat(lst).dropna()
+    fig = px.line(
+        sim_df,
+        x='days_passed',
+        y='circulating_supply',
+        title=f'{experiment} - AB Test Windowed Volatility of Circulating Supply',
+    )
+    return fig
