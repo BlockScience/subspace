fd835d27e2f4ef92150aa04d7ce9b495ddda116a
Shawn Anderson <shawn@longtailfinancial.com>
2024-01-03 20:57:50 -0800

    sanity_check notebook is now fully loaded from charts.py. Notebook renders as well as png exports are online with hvplot.

--- subspace_model/experiments/charts.py
+++ subspace_model/experiments/charts.py
@@ -135,7 +135,7 @@ def blockchain_size(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.Nd
     chart = sim_df.hvplot.line(x='days_passed', y=['history_size', 'space_pledged'], logy=True, width=1000, height=500, title='Blockchain Size', ylabel='Bytes')
     return chart
 
-def block_utilization(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.NdOverlay:
+def block_utilization(sim_df: pd.DataFrame, experiment: str) -> hv.element.chart.Curve:
     chart = sim_df.hvplot.line(x='days_passed', y=['block_utilization'], width=1000, height=500, title='Block Utilization')
     return chart
 
@@ -143,8 +143,8 @@ def non_negative_profits(profit1_timestep_df: pd.DataFrame) -> hv.core.overlay.N
     chart = profit1_timestep_df.loc[:, (profit1_timestep_df >= 0).all()].reset_index().hvplot.line(x='days_passed', logy=True, width=1000, height=500, title='Non Negative Profits')
     return chart
 
-def negative_profits(profit1_timestep_df: pd.DataFrame) -> hv.core.overlay.NdOverlay:
-    chart = profit1_timestep_df.loc[:, (profit1_timestep_df < 0).any()].reset_index().hvplot.line(x='days_passed', logy=True, width=1000, height=500, title='Non Negative Profits')
+def negative_profits(profit1_timestep_df: pd.DataFrame) -> hv.element.chart.Curve:
+    chart = profit1_timestep_df.loc[:, (profit1_timestep_df < 0).any()].reset_index().hvplot.line(x='days_passed', logy=True, width=1000, height=500, title='Negative Profits')
     return chart
 
 def holomap_selector_curve(profit1_timestep_df: pd.DataFrame) -> hv.core.overlay.NdOverlay:
@@ -161,7 +161,41 @@ def holomap_selector_box(profit1_timestep_df: pd.DataFrame) -> hv.core.overlay.N
     holomap = hv.HoloMap(curve_dict)
     return holomap
 
+def circulating_supply_volatility(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.NdOverlay:
+    s = window_volatility(sim_df.set_index('days_passed').circulating_supply.diff()).reset_index()
 
+    chart = s.hvplot.line(x='days_passed', 
+            y='circulating_supply',
+            title='Circulating Supply Volatility (z-score on weekly standard deviation)', width=1000, height=500)
+
+    return chart
+
+def weekly_rewards_to_nominators(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.NdOverlay:
+    n_days=7
+    fig_df = sim_df.set_index('days_passed')
+    s = fig_df.rewards_to_nominators.rolling(n_days).sum() / fig_df.circulating_supply.rolling(n_days).mean()
+    chart = s.hvplot.line(title="Weekly Rewards to Nominators (% of Circ Supply per day)", ylabel='Weekly Rewards to Nominators (% of Circulating Supply)', height=500, width=1000, yformatter='%.2f%%')
+
+    return chart
+
+def weekly_issuance_rate(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.NdOverlay:
+    n = 7
+    fig_df = sim_df.set_index('days_passed')
+    s = fig_df.reward_issuance_balance.diff() * -1.0 
+    s /= fig_df.max_credit_supply
+    s = s.rolling(n).sum()[n-1::n]
+    chart = s.hvplot(x='days_passed', y='reward_issuance_balance', title="Weekly Reward Issuance Rate", ylabel='% of Max Credit Supply ', yformatter='%.8f%%', height=500, width=1000)
+    return chart
+
+def cumulative_issuance_rate(sim_df: pd.DataFrame, experiment: str) -> hv.core.overlay.NdOverlay:
+    n = 7
+    fig_df = sim_df.set_index('days_passed')
+    s = fig_df.reward_issuance_balance.diff() * -1.0
+    s /= fig_df.max_credit_supply
+    s = s.cumsum()
+
+    chart = s.hvplot(x='days_passed', y='reward_issuance_balance', title="Cumulative Reward Issuance Rate", ylabel='% of Max Credit Supply ', yformatter='%.8f%%', height=500, width=1000)
+    return chart
 
 # def default_metrics(sim_df: pd.DataFrame, experiment: str) -> plotly.graph_objects.Figure:
 
