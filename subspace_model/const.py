ba430932ec573abe6ab66945563473bf30863d43
Shawn Anderson <shawn@longtailfinancial.com>
2024-02-05 15:15:34 -0800

    Checkpoint. Params, Structure spearated. Storage Fee spec updated.

--- subspace_model/const.py
+++ subspace_model/const.py
@@ -6,20 +6,21 @@ MIB_IN_BYTES = 1048576
 DAY_TO_SECONDS = 24 * 60 * 60
 
 # Subspace Constants
-MAX_CREDIT_ISSUANCE: Credits = 3_000_000_000   # TODO: check if true
-KZG_COMMITMENT: Bytes = 32   # As per Subnomicon
-WITNESS: Bytes = 48   # As per Subnomicon
-CHUNK_SIZE: Bytes = 48   # As per Subnomicon
-ARCHIVAL_DEPTH: Blocks = 100   # As per Subnomicon
-BLOCK_TIME: Seconds = 6   # As per Subnomicon. Approximate value.
-RAW_RECORD_IN_CHUNKS: Chunk = 2**15   # As per Subnomicon
+MAX_CREDIT_ISSUANCE: Credits = 3_000_000_000  # TODO: check if true
+ISSUANCE_FOR_FARMERS: Credits = MAX_CREDIT_ISSUANCE * 0.44
+KZG_COMMITMENT: Bytes = 32  # As per Subnomicon
+WITNESS: Bytes = 48  # As per Subnomicon
+CHUNK_SIZE: Bytes = 48  # As per Subnomicon
+ARCHIVAL_DEPTH: Blocks = 100  # As per Subnomicon
+BLOCK_TIME: Seconds = 6  # As per Subnomicon. Approximate value.
+RAW_RECORD_IN_CHUNKS: Chunk = 2**15  # As per Subnomicon
 PIECE_SIZE: Bytes = (
     RAW_RECORD_IN_CHUNKS * CHUNK_SIZE + KZG_COMMITMENT + WITNESS
-)   # As per Subnomicon
-SECTOR_IN_PIECES: Piece = 1_000   # As per Subnomicon
-SECTOR_SIZE = SECTOR_IN_PIECES * PIECE_SIZE   # ~= 1 MiB
-SEGMENT_SIZE: Bytes = 128 * MIB_IN_BYTES   # As per discussions
-SEGMENT_HISTORY_SIZE: Bytes = SEGMENT_SIZE * 2   # As per discussions
+)  # As per Subnomicon
+SECTOR_IN_PIECES: Piece = 1_000  # As per Subnomicon
+SECTOR_SIZE = SECTOR_IN_PIECES * PIECE_SIZE  # ~= 1 MiB
+SEGMENT_SIZE: Bytes = 128 * MIB_IN_BYTES  # As per discussions
+SEGMENT_HISTORY_SIZE: Bytes = SEGMENT_SIZE * 2  # As per discussions
 SHANNON_IN_CREDITS: Credits = 1e-18
 
 # Convenience Constants
--- subspace_model/types.py
+++ subspace_model/types.py
@@ -2,38 +2,38 @@ from dataclasses import dataclass
 from typing import Annotated, Callable, NamedTuple, Optional, TypedDict, Union
 
 # Time units
-Blocks = Annotated[float, 'blocks']  # 1 block ~ 6s. Or 1 day ~ 14400 blocks
-Days = Annotated[float, 'days']  # Number of days
-Seconds = Annotated[float, 's']
-PerYear = Annotated[float, '1/year']  # Yearly rate
-Year = Annotated[float, 'year']  # Number of years
+Blocks = Annotated[float, "blocks"]  # 1 block ~ 6s. Or 1 day ~ 14400 blocks
+Days = Annotated[float, "days"]  # Number of days
+Seconds = Annotated[float, "s"]
+PerYear = Annotated[float, "1/year"]  # Yearly rate
+Year = Annotated[float, "year"]  # Number of years
 
 # Measurement units
-Credits = Annotated[float, 'SSC']
-CreditsPerComputeWeights = Annotated[float, 'SSC/CW']
-CreditsPerDay = Annotated[float, 'SSC/day']
-ComputeWeights = Annotated[float, 'CW']
-Shannon = Annotated[float, 'Shannon']  # 1e-18 SSC
-ShannonPerComputeWeights = Annotated[float, 'Shannon/CW']
+Credits = Annotated[float, "SSC"]
+CreditsPerComputeWeights = Annotated[float, "SSC/CW"]
+CreditsPerDay = Annotated[float, "SSC/day"]
+ComputeWeights = Annotated[float, "CW"]
+Shannon = Annotated[float, "Shannon"]  # 1e-18 SSC
+ShannonPerComputeWeights = Annotated[float, "Shannon/CW"]
 
-Bytes = Annotated[int, 'bytes']
-Chunk = Annotated[int, 'chunk']  # As per Subnomicon: 1 chunk = 32 Bytes
-RawRecord = Annotated[Chunk, 'raw_record']  # As per Subnomicon: 2**15 Chunks (~1MB)
+Bytes = Annotated[int, "bytes"]
+Chunk = Annotated[int, "chunk"]  # As per Subnomicon: 1 chunk = 32 Bytes
+RawRecord = Annotated[Chunk, "raw_record"]  # As per Subnomicon: 2**15 Chunks (~1MB)
 Piece = Annotated[
-    int, 'piece'
+    int, "piece"
 ]  # As per Subnomicon: 1 piece = 1 record + commitment + witness
-Record = Annotated[Piece, 'record']  # As per Subnomicon: a transformed raw record.
-Sector = Annotated[Piece, 'sector']  # As per Subnomicon: 1000 Pieces or ~ 1 GiB
+Record = Annotated[Piece, "record"]  # As per Subnomicon: a transformed raw record.
+Sector = Annotated[Piece, "sector"]  # As per Subnomicon: 1000 Pieces or ~ 1 GiB
 
 # As per Subnomicon: A collection of potential partial or full blocks.
 # Can be either a fixed-size portion of the Blockchain History
 # or a fixed-size portion of the Archived History
-Segment = Annotated[Bytes, 'segment']
-RecordedHistorySegment = Annotated[Record, 'record_segment']
-ArchivedHistorySegment = Annotated[Piece, 'archive_segment']
+Segment = Annotated[Bytes, "segment"]
+RecordedHistorySegment = Annotated[Record, "record_segment"]
+ArchivedHistorySegment = Annotated[Piece, "archive_segment"]
 
 # Misc units
-Percentage = Annotated[float, '%']
+Percentage = Annotated[float, "%"]
 
 # Taxonomy:
 # Chunk < Record/Piece < Sector < Plot < History
@@ -72,8 +72,8 @@ class SubspaceModelState(TypedDict):
 
     # Deterministic Variables
     block_reward: Credits
-    history_size: Bytes
-    space_pledged: Bytes
+    blockchain_history_size: Bytes
+    total_space_pledged: Bytes
     allocated_tokens: Credits
     buffer_size: Bytes
 
@@ -108,7 +108,7 @@ class SubspaceModelParams(TypedDict):
     archival_depth: Blocks
     archival_buffer_segment_size: Bytes
     header_size: Bytes
-    replication_factor: float
+    min_replication_factor: float
     max_block_size: Bytes
 
     # Economic Parameters
