b445ed3f095cea95b9d41ead1ab7312e9ac462ee
Shawn Anderson <shawn@longtailfinancial.com>
2024-02-11 10:14:54 -0800

    Update max credit issuance and issuance for farmers.
    Update subspace_model/const.py
    
    Co-authored-by: Dariia Porechna <35382599+dariolina@users.noreply.github.com>

--- subspace_model/const.py
+++ subspace_model/const.py
@@ -7,7 +7,7 @@ DAY_TO_SECONDS = 24 * 60 * 60
 
 # Subspace Constants
 MAX_CREDIT_ISSUANCE: Credits = 3_000_000_000  # TODO: check if true
-ISSUANCE_FOR_FARMERS: Credits = MAX_CREDIT_ISSUANCE * 0.44
+ISSUANCE_FOR_FARMERS: Credits = MAX_CREDIT_ISSUANCE * 0.3782
 KZG_COMMITMENT: Bytes = 32  # As per Subnomicon
 WITNESS: Bytes = 48  # As per Subnomicon
 CHUNK_SIZE: Bytes = 48  # As per Subnomicon
