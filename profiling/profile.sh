44d7d18dbdec0e7c9db18d2f964073302eaf30aa
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-04-19 13:20:06 -0300

    sync

--- profiling/profile.sh
+++ profiling/profile.sh
@@ -2,6 +2,7 @@ OUTPUT_FOLDER=output
 
 for LABEL in psuu
 do
+    rm -rf $OUTPUT_FOLDER/$LABEL*
     python -m cProfile -o $OUTPUT_FOLDER/$LABEL.pstats run_$LABEL.py >> $OUTPUT_FOLDER/$LABEL.txt
     gprof2dot --colour-nodes-by-selftime -f pstats $OUTPUT_FOLDER/$LABEL.pstats | \
         dot -Tpng -o $OUTPUT_FOLDER/$LABEL.png
