2482075b9312905fd63656e3d2b9cfea58dd4031
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-04-23 20:15:39 -0300

    rm fund test

--- test/test_experiments.py
+++ test/test_experiments.py
@@ -16,10 +16,6 @@ def test_standard_stochastic_run():
     sim_df = standard_stochastic_run(SIMULATION_DAYS=70, TIMESTEP_IN_DAYS=1, SAMPLES=2)
 
 
-def test_fund_inclusion():
-    sim_df = fund_inclusion(SIMULATION_DAYS=70, TIMESTEP_IN_DAYS=1, SAMPLES=1)
-
-
 def test_reward_split_sweep():
     sim_df = reward_split_sweep(SIMULATION_DAYS=70, TIMESTEP_IN_DAYS=1, SAMPLES=1)
 
